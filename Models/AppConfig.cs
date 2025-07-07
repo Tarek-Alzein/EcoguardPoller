a691c12a01a1ac8590254fd8d7b3a9b1c7710d42
Tarek Alzein <tarek.alzein@norce.io>
2025-07-07 18:20:30 +0200

    removed default values and made them required

--- Models/AppConfig.cs
+++ Models/AppConfig.cs
@@ -2,30 +2,30 @@
 {
     internal class AppConfig
     {
-        public EcoGuardConfig EcoGuard { get; set; } = new();
-        public MqttConfig MQTT { get; set; } = new();
-        public DatabaseConfig Database { get; set; } = new();
+        public required EcoGuardConfig EcoGuard { get; set; }
+        public required MqttConfig MQTT { get; set; }
+        public required DatabaseConfig Database { get; set; }
         public double PollIntervalHours { get; set; } = 1;
     }
     public class EcoGuardConfig
     {
-        public string DomainCode { get; set; } = "";
-        public int NodeId { get; set; }
-        public string Username { get; set; } = "";
-        public string Password { get; set; } = "";
+        public required string DomainCode { get; set; }
+        public required int NodeId { get; set; }
+        public required string Username { get; set; }
+        public required string Password { get; set; }
     }
 
     public class MqttConfig
     {
-        public string Broker { get; set; } = "localhost";
-        public int Port { get; set; } = 1883;
-        public string Username { get; set; } = "";
-        public string Password { get; set; } = "";
-        public string Topic { get; set; } = "ecoguard/consumption";
+        public required string Broker { get; set; }
+        public int Port { get; set; }
+        public string? Username { get; set; }
+        public string? Password { get; set; }
+        public required string Topic { get; set; }
     }
     public class DatabaseConfig
     {
-        public string Path { get; set; } = "/consumption.db";
+        public required string Path { get; set; }
     }
 
 }
